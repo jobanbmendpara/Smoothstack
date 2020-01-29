755ea74659301ffd49318040b729c1b640a9d023
Joban Mendpara <jobanmendpara@gmail.com>
2020-01-29 13:16:57 -0500

    Comment for clarity

--- Week1/src/com/smoothstack/week1/assignments2/Array2D.java
+++ Week1/src/com/smoothstack/week1/assignments2/Array2D.java
@@ -20,6 +20,7 @@ public class Array2D {
 		Scanner input = new Scanner(System.in);
 		int[][] fillArray = new int[3][3];
 		
+		// Populate Array
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
 				System.out.print("Enter a value for index (" + i + ", " + j + "): ");
